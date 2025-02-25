6b5a67e3a81df623d09ff8ab9504d893fabcc327
bungongtoto <bungongkingsley@gmail.com>
2025-02-25 11:46:20 +0000

    add contacts and appointments state and callback functions to add new contact and appointmnet objects to the array states

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
@@ -9,22 +9,34 @@ function App() {
   Define state variables for 
   contacts and appointments 
   */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
   /*
   Implement functions to add data to
   contacts and appointments
   */
 
+  const addNewContact = (name, phone, number, email) => {
+    const newContact = { name, phone, number, email };
+    setContacts((prevContacts) => [newContact, ...prevContacts]);
+  }
+
+  const addNewAppointment = (name, contact, date, time) => {
+    const newAppointment = { name, contact, date, time };
+    setAppointments((prevAppointments) => [newAppointment, ...prevAppointments]);
+  }
+
   const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={ <Root/> }>
-      <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
+    <Route path="/" element={<Root />}>
+      <Route index element={<Navigate to={ROUTES.CONTACTS} replace />} />
+      <Route path={ROUTES.CONTACTS} element={<ContactsPage contacts={contacts} addNewContact={addNewContact} /> /* Add props to ContactsPage */} />
+      <Route path={ROUTES.APPOINTMENTS} element={<AppointmentsPage appointments={appointments} addNewAppointment={addNewAppointment} /> /* Add props to AppointmentsPage */} />
     </Route>
   ));
-  
+
   return (
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   );
 }
 
