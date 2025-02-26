c9f16274b0f8d051535fec6f27d5a62b5b13d7be
bungongtoto <bungongkingsley@gmail.com>
2025-02-26 10:21:58 +0000

    add tileList

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
+
 
 export const TileList = ({arrayObject}) => {
   return (
     <div>
-      
+      {
+        arrayObject.map(({name,...rest}, index) => {
+          return <Tile key={index} name={name} description={rest} />
+        })
+      }
     </div>
   );
 };
